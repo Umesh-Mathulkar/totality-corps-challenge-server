47e16fbe2fdd8ea5196fd6be5fbb5b9ba6191903
Umesh-Mathulkar <mathulkar98@gmail.com>
2023-08-28 16:13:17 +0530

    updated

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 const passportJWT = require('passport-jwt');
-const User = require('../models/user');
+const User = require('../models/User');
 
 const JwtStrategy = passportJWT.Strategy;
 const ExtractJwt = passportJWT.ExtractJwt;
